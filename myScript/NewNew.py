c032436624ff604065534ba73ebf388d9aea8b01
Rovo-3 <rovostech1@outlook.com>
2024-10-04 16:18:21 +0700

    save milestone, idk which

--- myScript/NewNew.py
+++ myScript/NewNew.py
@@ -16,19 +16,21 @@ from stitching.blender import Blender
 # My Code ==================================================================================================================================================================
 from natsort import natsorted
 import glob
+
 # My Code ==================================================================================================================================================================
+
 
 def plot_image(img, figsize_in_inches=(5, 5)):
     fig, ax = plt.subplots(figsize=figsize_in_inches)
     ax.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
-    plt.show()
+    # plt.show()
 
 
 def plot_images(imgs, figsize_in_inches=(5, 5)):
     fig, axs = plt.subplots(1, len(imgs), figsize=figsize_in_inches)
     for col, img in enumerate(imgs):
         axs[col].imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
-    plt.show()
+    # plt.show()
 
 
 def get_image_paths(img_set):
@@ -41,15 +43,17 @@ def wb_opencv(img):
     wb_img = wb.balanceWhite(img)
     return wb_img
 
+
 def chanelClahe(channel):
     clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(4, 4))
     channel_clahe = clahe.apply(channel)
     return channel_clahe
 
+
 def imagePreProcess(imagetobeprocessed):
     white_balanced_img = wb_opencv(imagetobeprocessed)
     imagesprocessed = imagetobeprocessed
-    
+
     lab_image = cv.cvtColor(imagesprocessed, cv.COLOR_BGR2LAB)
     l_channel, a_channel, b_channel = cv.split(lab_image)
 
@@ -59,14 +63,17 @@ def imagePreProcess(imagetobeprocessed):
     final_img_lab = cv.cvtColor(merged_lab, cv.COLOR_LAB2BGR)
     return final_img_lab
 
+
 def resizeImage(img, scale):
     h, w, _ = img.shape
-    h = h*scale
-    w = w*scale
+    h = h * scale
+    w = w * scale
     # print (image.shape)
     # print("Height: ", h, "; Width: ", w)
-    img = cv.resize(img, (int(w) , int(h)))
+    img = cv.resize(img, (int(w), int(h)))
     return img
+
+
 # My Code ==================================================================================================================================================================
 
 # ==========================================================================================================================================================================
@@ -75,25 +82,26 @@ budapest_imgs = get_image_paths("buda")
 exposure_error_imgs = get_image_paths("exp")
 barcode_imgs = get_image_paths("barc")
 barcode_masks = get_image_paths("mask")
-# office_imgs = [
-#     # str(path.relative_to(".")) for path in Path("../Images/officemanyimages").rglob(f"*")
-# ]
+office_imgs = [
+    str(path.relative_to("."))
+    for path in Path("../myScript/dumdum").rglob(f"*")
+]
 
 # My Code ==================================================================================================================================================================
-imagesarr = []
-# imagePaths = natsorted(list(glob.glob("../Images/vidframes/*")))
-imagePaths = natsorted(list(glob.glob("./dummy/*")))
+# imagesarr = []
+# # imagePaths = natsorted(list(glob.glob("../Images/vidframes/*")))
+# imagePaths = natsorted(list(glob.glob("./dummy/*")))
 
-for imagePath in imagePaths:
-    image = cv.imread(imagePath)
-    image = resizeImage(image, 1)
-    
-    processedimage = imagePreProcess(image)
-    imagesarr.append(processedimage)
-    print(imagePath)
+# for imagePath in imagePaths:
+#     image = cv.imread(imagePath)
+#     image = resizeImage(image, 1)
+
+#     processedimage = imagePreProcess(image)
+#     imagesarr.append(processedimage)
+#     print(imagePath)
 # My Code ==================================================================================================================================================================
 # ==========================================================================================================================================================================
-images = Images.of(imagesarr)
+images = Images.of(office_imgs)
 
 medium_imgs = list(images.resize(Images.Resolution.MEDIUM))
 low_imgs = list(images.resize(Images.Resolution.LOW))
@@ -125,7 +133,12 @@ matches = matcher.match_features(features)
 matcher.get_confidence_matrix(matches)
 # ==========================================================================================================================================================================
 all_relevant_matches = matcher.draw_matches_matrix(
-    medium_imgs, features, matches, conf_thresh=0.2, inliers=True, matchColor=(0, 255, 0)
+    medium_imgs,
+    features,
+    matches,
+    conf_thresh=0.2,
+    inliers=True,
+    matchColor=(0, 255, 0),
 )
 
 for idx1, idx2, img in all_relevant_matches:
@@ -166,7 +179,9 @@ warper = Warper()
 warper.set_scale(cameras)
 # ==========================================================================================================================================================================
 low_sizes = images.get_scaled_img_sizes(Images.Resolution.LOW)
-camera_aspect = images.get_ratio(Images.Resolution.MEDIUM, Images.Resolution.LOW)  # since cameras were obtained on medium imgs
+camera_aspect = images.get_ratio(
+    Images.Resolution.MEDIUM, Images.Resolution.LOW
+)  # since cameras were obtained on medium imgs
 
 warped_low_imgs = list(warper.warp_images(low_imgs, cameras, camera_aspect))
 warped_low_masks = list(warper.create_and_warp_masks(low_sizes, cameras, camera_aspect))
@@ -176,27 +191,31 @@ final_sizes = images.get_scaled_img_sizes(Images.Resolution.FINAL)
 camera_aspect = images.get_ratio(Images.Resolution.MEDIUM, Images.Resolution.FINAL)
 
 warped_final_imgs = list(warper.warp_images(final_imgs, cameras, camera_aspect))
-warped_final_masks = list(warper.create_and_warp_masks(final_sizes, cameras, camera_aspect))
+warped_final_masks = list(
+    warper.create_and_warp_masks(final_sizes, cameras, camera_aspect)
+)
 final_corners, final_sizes = warper.warp_rois(final_sizes, cameras, camera_aspect)
 # ==========================================================================================================================================================================
-plot_images(warped_low_imgs, (10,10))
-plot_images(warped_low_masks, (10,10))
+plot_images(warped_low_imgs, (10, 10))
+plot_images(warped_low_masks, (10, 10))
 # ==========================================================================================================================================================================
 print(final_corners)
 print(final_sizes)
 # ==========================================================================================================================================================================
-timelapser = Timelapser('as_is')
+timelapser = Timelapser("as_is")
 timelapser.initialize(final_corners, final_sizes)
 
 for img, corner in zip(warped_final_imgs, final_corners):
     timelapser.process_frame(img, corner)
     frame = timelapser.get_frame()
-    plot_image(frame, (10,10))
+    plot_image(frame, (10, 10))
 # ==========================================================================================================================================================================
 cropper = Cropper()
 # ==========================================================================================================================================================================
-mask = cropper.estimate_panorama_mask(warped_low_imgs, warped_low_masks, low_corners, low_sizes)
-plot_image(mask, (5,5))
+mask = cropper.estimate_panorama_mask(
+    warped_low_imgs, warped_low_masks, low_corners, low_sizes
+)
+plot_image(mask, (5, 5))
 # ==========================================================================================================================================================================
 lir = cropper.estimate_largest_interior_rectangle(mask)
 # ==========================================================================================================================================================================
@@ -204,21 +223,21 @@ lir = cropper.estimate_largest_interior_rectangle(mask)
 print(lir)
 # ==========================================================================================================================================================================
 plot = lir.draw_on(mask, size=2)
-plot_image(plot, (5,5))
+plot_image(plot, (5, 5))
 # ==========================================================================================================================================================================
 low_corners = cropper.get_zero_center_corners(low_corners)
 rectangles = cropper.get_rectangles(low_corners, low_sizes)
 
 plot = rectangles[1].draw_on(plot, (0, 255, 0), 2)  # The rectangle of the center img
-plot_image(plot, (5,5))
+plot_image(plot, (5, 5))
 # ==========================================================================================================================================================================
 overlap = cropper.get_overlap(rectangles[1], lir)
 plot = overlap.draw_on(plot, (255, 0, 0), 2)
-plot_image(plot, (5,5))
+plot_image(plot, (5, 5))
 # ==========================================================================================================================================================================
 intersection = cropper.get_intersection(rectangles[1], overlap)
 plot = intersection.draw_on(warped_low_masks[1], (255, 0, 0), 2)
-plot_image(plot, (2.5,2.5))
+plot_image(plot, (2.5, 2.5))
 # ==========================================================================================================================================================================
 cropper.prepare(warped_low_imgs, warped_low_masks, low_corners, low_sizes)
 
@@ -226,34 +245,45 @@ cropped_low_masks = list(cropper.crop_images(warped_low_masks))
 cropped_low_imgs = list(cropper.crop_images(warped_low_imgs))
 low_corners, low_sizes = cropper.crop_rois(low_corners, low_sizes)
 
-lir_aspect = images.get_ratio(Images.Resolution.LOW, Images.Resolution.FINAL)  # since lir was obtained on low imgs
+lir_aspect = images.get_ratio(
+    Images.Resolution.LOW, Images.Resolution.FINAL
+)  # since lir was obtained on low imgs
 cropped_final_masks = list(cropper.crop_images(warped_final_masks, lir_aspect))
 cropped_final_imgs = list(cropper.crop_images(warped_final_imgs, lir_aspect))
 final_corners, final_sizes = cropper.crop_rois(final_corners, final_sizes, lir_aspect)
 # ==========================================================================================================================================================================
-timelapser = Timelapser('as_is')
+timelapser = Timelapser("as_is")
 timelapser.initialize(final_corners, final_sizes)
 
 for img, corner in zip(cropped_final_imgs, final_corners):
     timelapser.process_frame(img, corner)
     frame = timelapser.get_frame()
-    plot_image(frame, (10,10))
+    plot_image(frame, (10, 10))
 # ==========================================================================================================================================================================
 seam_finder = SeamFinder()
 
 seam_masks = seam_finder.find(cropped_low_imgs, low_corners, cropped_low_masks)
-seam_masks = [seam_finder.resize(seam_mask, mask) for seam_mask, mask in zip(seam_masks, cropped_final_masks)]
+seam_masks = [
+    seam_finder.resize(seam_mask, mask)
+    for seam_mask, mask in zip(seam_masks, cropped_final_masks)
+]
 
-seam_masks_plots = [SeamFinder.draw_seam_mask(img, seam_mask) for img, seam_mask in zip(cropped_final_imgs, seam_masks)]
-plot_images(seam_masks_plots, (15,10))
+seam_masks_plots = [
+    SeamFinder.draw_seam_mask(img, seam_mask)
+    for img, seam_mask in zip(cropped_final_imgs, seam_masks)
+]
+plot_images(seam_masks_plots, (15, 10))
 # ==========================================================================================================================================================================
 compensator = ExposureErrorCompensator()
 
 compensator.feed(low_corners, cropped_low_imgs, cropped_low_masks)
 
-compensated_imgs = [compensator.apply(idx, corner, img, mask) 
-                    for idx, (img, mask, corner) 
-                    in enumerate(zip(cropped_final_imgs, cropped_final_masks, final_corners))]
+compensated_imgs = [
+    compensator.apply(idx, corner, img, mask)
+    for idx, (img, mask, corner) in enumerate(
+        zip(cropped_final_imgs, cropped_final_masks, final_corners)
+    )
+]
 # ==========================================================================================================================================================================
 blender = Blender()
 blender.prepare(final_corners, final_sizes)
@@ -261,13 +291,15 @@ for img, mask, corner in zip(compensated_imgs, seam_masks, final_corners):
     blender.feed(img, mask, corner)
 panorama, _ = blender.blend()
 # ==========================================================================================================================================================================
-plot_image(panorama, (20,20))
+# plot_image(panorama, (20, 20))
 # figsize_in_inches=(20,20)
 
 # fig, ax = plt.subplots(figsize=figsize_in_inches)
 # ax.imshow(cv.cvtColor(panorama, cv.COLOR_BGR2RGB))
-# cv.imshow(cv.cvtColor(panorama, cv.COLOR_BGR2RGB))
-# cv.waitKey(0)
+# plt.show()
+cv.imshow("last", panorama)
+cv.imwrite("./bruh.jpg", panorama)
+cv.waitKey(0)
 # # ==========================================================================================================================================================================
 # blended_seam_masks = seam_finder.blend_seam_masks(seam_masks, final_corners, final_sizes)
 # plot_image(blended_seam_masks, (5,5))
--- myScript/master.py
+++ myScript/master.py
@@ -6,17 +6,30 @@ import datetime
 from natsort import natsorted
 import glob
 
-outpath = "../Results/" + datetime.datetime.now().strftime("%m-%d-%Y-%H-%M-%S") + "_stitched.png"
+outpath = (
+    "../Results/"
+    + datetime.datetime.now().strftime("%m-%d-%Y-%H-%M-%S")
+    + "_stitched.png"
+)
+
+
+def pyforceClose():
+    import sys
+
+    sys.exit()
+
 
 def wb_opencv(img):
     wb = cv2.xphoto.createSimpleWB()
     wb_img = wb.balanceWhite(img)
     return wb_img
 
+
 def chanelClahe(channel):
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4, 4))
     channel_clahe = clahe.apply(channel)
     return channel_clahe
+
 
 def imagePreProcess(imagetobeprocessed):
     white_balanced_img = wb_opencv(imagetobeprocessed)
@@ -30,59 +43,88 @@ def imagePreProcess(imagetobeprocessed):
     final_img_lab = cv2.cvtColor(merged_lab, cv2.COLOR_LAB2BGR)
     return final_img_lab
 
+
 def resizeImage(img, scale):
     h, w, _ = img.shape
-    h = h*scale
-    w = w*scale
+    h = h * scale
+    w = w * scale
     # print (image.shape)
     # print("Height: ", h, "; Width: ", w)
-    img = cv2.resize(img, (int(w) , int(h)))
+    img = cv2.resize(img, (int(w), int(h)))
     return img
 
+
 print("[INFO] loading images...")
-imagePaths = natsorted(list(glob.glob("../Images/vidframes/*")))
+# imagePaths = natsorted(list(glob.glob("../Images/vidframes/*")))
+imagePaths = natsorted(list(glob.glob("./dumdum/*")))
+
+totalImages = len(imagePaths)
+
+images = []
+# lastimagename = ""
+# counting = 0
+
+# IDEA:
+# read total images
+# if images > 20
+# make segments of 20 pics
+# process by taking 20 pics and stitch it.
+# the next 20 pics will continue, and so on..
+# after each segment is done, the stitched result will be stitched along with other stitched result.
+
+# while True:
+#     images.clear()
+
+#     if totalImages > 20:
+#         for imagePath in imagePaths:
+#             image = cv2.imread(imagePath)
+#             image = resizeImage(image, 1)
+
+#             processedimage = imagePreProcess(image)
+#             images.append(processedimage)
+#             counting += 1
+#             print(counting)
+#             print(imagePath)
+
+#             if counting >= 20:
+#                 lastimagename = imagePath
+#                 print(lastimagename)
+#                 break
+#         # print()
+#     if lastimagename is not None:
+#         break
+#     print("Images Processed")
+
+# pyforceClose()
 
 if __name__ == "__main__":
     images = []
-    
-    # arr1 = []
-    # arr2 = []
-    # count = 0
-    # for i in range(10):
-    #     arr1.append(i)
-    #     count += 1
-    #     if count == 2:
-    #         data = arr1[(len(arr1)-2)] + arr1[(len(arr1)-1)]
-    #         print(data)
-    #         arr2.append(data)
-    #         arr1.clear()
-    #         count = 0
-    # print(arr1)
-    # print(arr2)
+
     for imagePath in imagePaths:
         image = cv2.imread(imagePath)
-        image = resizeImage(image, 1)
-        
-        processedimage = imagePreProcess(image)
-        images.append(processedimage)
+        # image = resizeImage(image, 1)
+
+        # processedimage = imagePreProcess(image)
+        images.append(image)
         print(imagePath)
-    
+
     print("[INFO] stitching images...")
     # scan = cv2.Stitcher_SCANS
     # panorama = cv2.Stitcher_PANORAMA
     # (status, stitched) = cv2.Stitcher.create(scan).stitch(images)
-    
-    settings = {"detector": "sift", 
-            "confidence_threshold": 0.5, 
-            # "blender_type": "feather",
-            "crop": False,
-            # "no-crop": "True",
-            # "wave_correct_kind": "auto"
-            "try_use_gpu": True
-            }
+
+    settings = {
+        "detector": "sift"
+        # "confidence_threshold": 0.5,
+        # "blender_type": "feather",
+        # "crop": False,
+        # "no-crop": "True",
+        # "wave_correct_kind": "auto"
+        # "try_use_gpu": True
+    }
     stitcher = Stitcher(**settings)
     stitched = stitcher.stitch(images)
-    
+
     # if status == 0:
     # try:
     print("[INFO] save stitching...")
@@ -96,4 +138,4 @@ if __name__ == "__main__":
 
     # except:
     # else:
-        # print("[INFO] image stitching failed ({})".format(status))
+    # print("[INFO] image stitching failed ({})".format(status))
